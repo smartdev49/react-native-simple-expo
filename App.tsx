451e1d4b4ec5044345fc54d799de8659a212e9a0
unknown <curtisjowen@gmail.com>
2024-11-03 18:02:44 -0500

    drawer & tab

--- App.tsx
+++ App.tsx
@@ -5,12 +5,29 @@ import { RoutesParams } from './router';
 import Settings from './Settings';
 import Home from './Home';
 import Details from './Details';
-import { Button } from 'react-native';
+import { Button, Platform } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+const Tab = createBottomTabNavigator<RoutesParams>();
 const Stack = createNativeStackNavigator<RoutesParams>();
+const Drawer = createDrawerNavigator<RoutesParams>();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      {Platform.OS === 'ios' &&
+          (<Tab.Navigator>
+            <Tab.Screen name='Home' component={Home} />
+            <Tab.Screen name='Settings' component={Settings} />
+            <Tab.Screen name='Details' component={Details} />
+          </Tab.Navigator>)
+        }
+      {Platform.OS === "android" && 
+        <Drawer.Navigator>
+          <Drawer.Screen name='Home' component={Home} />
+          <Drawer.Screen name='Settings' component={Settings} />
+          <Drawer.Screen name='Details' component={Details} />
+        </Drawer.Navigator>
+      }
         <Stack.Screen name='Home' component={Home} />
         <Stack.Screen name='Settings' component={Settings} />
         <Stack.Screen name='Details' component={Details}
@@ -24,7 +41,6 @@ export default function App() {
               )
             }
           })} />
-      </Stack.Navigator>
     </NavigationContainer>
   );
 }
